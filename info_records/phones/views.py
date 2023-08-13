e7fe076eb8e1852917cc5c1306dc22f1bcc1b72c
mehranredrose <mehranredrose@gmail.com>
2023-08-13 22:21:11 +0330

    add report view

--- info_records/phones/views.py
+++ info_records/phones/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import AddPhoneForm
 from django.http import HttpResponse
-
+import json
+from .reports import all_brands_in_korea, some_brand_mobiles, mobiles_brand_nation_equals_made_in 
 #from .models import Phone
 # Create your views here.
 def add_phone(request):
@@ -17,3 +18,9 @@ def add_phone(request):
     #we can write this  CreateAPIView
     
     
+def report(request):
+    response_data = {}
+    response_data['all_brands_in_korea'] = all_brands_in_korea()
+    response_data['some_brand_mobiles'] = some_brand_mobiles()
+    response_data['mobiles_brand_nation_equals_made_in'] = mobiles_brand_nation_equals_made_in()
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
